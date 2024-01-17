9d569e843a5cf26872c2f28d68e326fbc91f03fb
ImanolSuppoMELI <imanol.suppo@mercadolibre.com>
2024-01-17 10:13:48 -0300

    [feat] discard changes

--- src/main/java/org/be_java_hisp_w24_g05/dto/UserFollowedDto.java
+++ src/main/java/org/be_java_hisp_w24_g05/dto/UserFollowedDto.java
@@ -2,14 +2,12 @@ package org.be_java_hisp_w24_g05.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 public record UserFollowedDto(
         @JsonProperty("user_id")
         Integer userId,
         @JsonProperty("user_name")
         String userName,
-        @JsonProperty("quantity_of_followed")
+        @JsonProperty("quantity_of_followers")
         int quantityOfFollowed
-){
-}
+        ){};
 
