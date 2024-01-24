dbf136a39e89af1d1221d82a409ca4d018d93c67
jzapatamacias <juan.zapatamacias@mercadolibre.com.co>
2024-01-24 15:42:41 -0500

    [FEAT]added new validations to the PostDto and the productDto

--- src/main/java/org/be_java_hisp_w24_g05/dto/PostDto.java
+++ src/main/java/org/be_java_hisp_w24_g05/dto/PostDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import jakarta.validation.constraints.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
@@ -16,7 +17,11 @@ public record PostDto(
         @JsonProperty("user_id") Integer userId,
         String date,
         ProductDto product,
+
+        @NotNull(message = "El campo no puede estar vacío.")
         Integer category,
+        @NotNull(message = "El campo no puede estar vacío.")
+        @DecimalMax(value = "10000000", message = "El precio máximo por producto es de 10.000.000")
         Double price
 ) {
 }
--- src/main/java/org/be_java_hisp_w24_g05/dto/ProductDto.java
+++ src/main/java/org/be_java_hisp_w24_g05/dto/ProductDto.java
@@ -1,13 +1,24 @@
 package org.be_java_hisp_w24_g05.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record ProductDto(
         @JsonProperty("product_id") Integer productId,
         @JsonProperty("product_name") String productName,
         String type,
+        @Size(max = 25,message = "La longitud no puede superar los 25 caracteres.")
+        @NotBlank(message = "El campo no puede estar vacío.")
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "El campo no puede poseer caracteres especiales.")
         String brand,
+        @Size(max = 15,message = "La longitud no puede superar los 15 caracteres.")
+        @NotBlank(message = "El campo no puede estar vacío.")
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "El campo no puede poseer caracteres especiales.")
         String color,
+        @Size(max = 80,message = "La longitud no puede superar los 80 caracteres.")
+        @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "El campo no puede poseer caracteres especiales.")
         String notes
 ) {
 }
