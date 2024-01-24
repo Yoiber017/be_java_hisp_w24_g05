0ba239a5da26fc541bc8ac2fa3c5f9bac96179d7
ImanolSuppoMELI <imanol.suppo@mercadolibre.com>
2024-01-24 15:47:35 -0300

    [fix] add Class ModelMapper, move convert methods in ModelMapper

--- src/main/java/org/be_java_hisp_w24_g05/service/UserService.java
+++ src/main/java/org/be_java_hisp_w24_g05/service/UserService.java
@@ -1,21 +1,17 @@
 package org.be_java_hisp_w24_g05.service;
 
+import org.be_java_hisp_w24_g05.common.ModelMapper;
 import org.be_java_hisp_w24_g05.dto.CountFollowersDto;
 import org.be_java_hisp_w24_g05.entity.User;
 import org.be_java_hisp_w24_g05.exception.NotFoundException;
 
 import org.be_java_hisp_w24_g05.dto.*;
 import org.be_java_hisp_w24_g05.entity.Post;
-import org.be_java_hisp_w24_g05.entity.Product;
 import org.be_java_hisp_w24_g05.repository.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -27,10 +23,12 @@ public class UserService implements IUserService {
     @Autowired
     private IUserRepository userRepository;
 
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Override
     public CountFollowersDto searchUserFollowers(Integer userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
-        return userToCountFollowersDTO(user);
+        return modelMapper.userToCountFollowersDTO(user);
     }
 
 
@@ -49,13 +47,13 @@ public class UserService implements IUserService {
     @Override
     public UserFollowedDto followUser(int userId, int userIdToFollow) {
         User user = this.userRepository.addFollower(userId, userIdToFollow);
-        return userToUserFollowedDTO(user);
+        return modelMapper.userToUserFollowedDTO(user);
     }
 
     @Override
     public UserFollowedDto unfollowUser(int userId, int userIdToUnfollow) {
         User user = this.userRepository.removeFollower(userId, userIdToUnfollow);
-        return userToUserFollowedDTO(user);
+        return modelMapper.userToUserFollowedDTO(user);
     }
     @Override
     public List<UserFollowersDto> searchUserFollowers(Integer userId, String order) {
@@ -67,8 +65,8 @@ public class UserService implements IUserService {
         }else if(order.equals("name_desc")){
             users.sort(Comparator.comparing(User::getUserName).reversed());
         }
-        return Collections.singletonList(convertUserFollowersToDto(user, users.stream()
-                .map(this::convertUserToDto)
+        return Collections.singletonList(modelMapper.convertUserFollowersToDto(user, users.stream()
+                .map(modelMapper::convertUserToDto)
                 .collect(Collectors.toList())));
 
     }
@@ -83,48 +81,11 @@ public class UserService implements IUserService {
             followedList.sort(Comparator.comparing(User::getUserName).reversed());
         }
 
-        return Collections.singletonList(convertUserFollowedToDto(user, followedList.stream()
-                .map(this::convertUserToDto)
+        return Collections.singletonList(modelMapper.convertUserFollowedToDto(user, followedList.stream()
+                .map(modelMapper::convertUserToDto)
                 .collect(Collectors.toList())));
 
     }
-    private UserDto convertUserToDto(User u){
-        return new UserDto(
-                u.getUserId(),
-                u.getUserName()
-        );
-    }
 
-    private UserFollowersDto convertUserFollowersToDto(User u,List<UserDto> users){
-        return new UserFollowersDto(
-                u.getUserId(),
-                u.getUserName(),
-                users
-        );
-    }
-
-    private UserFollowedByDto convertUserFollowedToDto(User u,List<UserDto> users){
-        return new UserFollowedByDto(
-                u.getUserId(),
-                u.getUserName(),
-                users
-        );
-    }
-
-    private UserFollowedDto userToUserFollowedDTO(User user) {
-        return new UserFollowedDto(
-                user.getUserId(),
-                user.getUserName(),
-                user.getFollowed().size()
-        );
-    }
-
-    private CountFollowersDto userToCountFollowersDTO(User user) {
-        return new CountFollowersDto(
-                user.getUserId(),
-                user.getUserName(),
-                user.getFollowers().size()
-        );
-    }
 
 }
