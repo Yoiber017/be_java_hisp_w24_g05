c3ed1f17fa03396821b876e73ced38a0ac4e4391
ImanolSuppoMELI <imanol.suppo@mercadolibre.com>
2024-01-25 16:06:08 -0300

    [feat] add unit tests T-0007

--- src/test/java/org/be_java_hisp_w24_g05/repository/UserRepositoryTest.java
+++ src/test/java/org/be_java_hisp_w24_g05/repository/UserRepositoryTest.java
@@ -1,13 +1,18 @@
 package org.be_java_hisp_w24_g05.repository;
 
 import org.be_java_hisp_w24_g05.common.Data;
+import org.be_java_hisp_w24_g05.entity.Post;
+import org.be_java_hisp_w24_g05.entity.Product;
 import org.be_java_hisp_w24_g05.entity.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public class UserRepositoryTest {
 
@@ -25,4 +30,24 @@ public class UserRepositoryTest {
 
         Assertions.assertEquals(expected, result);
     }
+
+    @Test
+    @DisplayName("Find user by id")
+    public void findById() {
+        User expected = data.loadData().get(0);
+
+        Optional<User> result = userRepository.findById(1);
+
+        Assertions.assertEquals(expected, result.get());
+    }
+
+    @Test
+    @DisplayName("Find user by id not found")
+    public void findByIdNotFound() {
+        Optional<User> result = userRepository.findById(100);
+
+        Assertions.assertEquals(Optional.empty(), result);
+    }
+
+
 }
--- src/test/java/org/be_java_hisp_w24_g05/service/UserServiceTest.java
+++ src/test/java/org/be_java_hisp_w24_g05/service/UserServiceTest.java
@@ -1,11 +1,21 @@
 package org.be_java_hisp_w24_g05.service;
 
 
+import org.be_java_hisp_w24_g05.common.Data;
+import org.be_java_hisp_w24_g05.dto.CountFollowersDto;
+import org.be_java_hisp_w24_g05.entity.User;
+import org.be_java_hisp_w24_g05.exception.NotFoundException;
 import org.be_java_hisp_w24_g05.repository.IUserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -15,5 +25,75 @@ public class UserServiceTest {
 
     @InjectMocks
     private UserService userService;
+
+    private final Data data = new Data();
+
+    @Test
+    @DisplayName("[T-0007] - Verificar que la cantidad de seguidores de un determinado usuario sea correcta. (US-0002) - Usuario con id 1")
+    public void searchUserFollowersId1() {
+
+        CountFollowersDto expected = new CountFollowersDto(1, "User1", 2);
+
+        User user = data.loadData().get(0);
+
+        Mockito.when(userRepository.findById(1)).thenReturn(Optional.of(user));
+
+        CountFollowersDto result = userService.searchUserFollowers(1);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("[T-0007] - Verificar que la cantidad de seguidores de un determinado usuario sea correcta. (US-0002) - Usuario con id 2")
+    public void searchUserFollowersId2() {
+
+        CountFollowersDto expected = new CountFollowersDto(2, "User2", 0);
+
+        User user = data.loadData().get(1);
+
+        Mockito.when(userRepository.findById(2)).thenReturn(Optional.of(user));
+
+        CountFollowersDto result = userService.searchUserFollowers(2);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("[T-0007] - Verificar que la cantidad de seguidores de un determinado usuario sea correcta. (US-0002) - Usuario con id 3")
+    public void searchUserFollowersId3() {
+
+        CountFollowersDto expected = new CountFollowersDto(3, "User3", 0);
+
+        User user = data.loadData().get(2);
+
+        Mockito.when(userRepository.findById(3)).thenReturn(Optional.of(user));
+
+        CountFollowersDto result = userService.searchUserFollowers(3);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("[T-0007] - Verificar que la cantidad de seguidores de un determinado usuario sea correcta. (US-0002) - Usuario con id 4")
+    public void searchUserFollowersId4() {
+
+        CountFollowersDto expected = new CountFollowersDto(4, "User4", 0);
+
+        User user = data.loadData().get(3);
+
+        Mockito.when(userRepository.findById(4)).thenReturn(Optional.of(user));
+
+        CountFollowersDto result = userService.searchUserFollowers(4);
+
+        Assertions.assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("[T-0007] - Verificar que la cantidad de seguidores de un determinado usuario sea correcta. (US-0002) - Usuario no registrado")
+    public void searchUserFollowersNotFound() {
+
+        Mockito.when(userRepository.findById(10)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoundException.class, () -> userService.searchUserFollowers(10));
+    }
 
 }
